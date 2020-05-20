76d35d0f106daed40ada5a1fa746b4b0903ae945
max77p <shanindrakumar@gmail.com>
2020-05-20 10:09:19 -0400

    added redux and actions

---
+++ src/actions/index.test.js
@@ -0,0 +1,8 @@
+import {correctGuess,actionTypes} from './';
+
+describe('correctGuess',()=>{
+    test('returns an action with type correctGuess',()=>{
+        const action = correctGuess();
+        expect(action).toEqual({type: actionTypes.CORRECT_GUESS})
+    })
+})
